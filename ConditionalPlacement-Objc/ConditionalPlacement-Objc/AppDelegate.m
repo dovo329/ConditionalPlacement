edaa4486545623abbf781d76c913fa72956efdd4
Doug Voss <douglas.c.voss@gmail.com>
2015-03-14 21:55:29 -0600

    added karl and leonard conditional compare code

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -18,7 +18,22 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    int karl = 10;
+    int leonard = 7;
     
+    int karlPlace = (karl > 7) ? 1 : 2;
+    int leonardPlace = (leonard > 7) ? 1 : 2;
+    
+    if (karlPlace == 1 && leonardPlace == 1) {
+        
+        NSLog(@"Both teachers placed one");
+    } else if (karlPlace == 1) {
+        NSLog(@"Karl is placed one and Leonard is placed two");
+    } else if (leonardPlace == 1) {
+        NSLog(@"Leonard is placed one and Karl is placed two");
+    } else {
+        NSLog(@"Karl and Leonard are placed two");
+    }
     
     return YES;
 }
